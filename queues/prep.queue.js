bdf338e3fece7e94db2df6c9b8a9b6e7d356f747
rustamtm <42986041+rustamtm@users.noreply.github.com>
2025-08-11 16:16:02 -0400

    chore: remove obsolete bullmq scheduler

--- queues/prep.queue.js
+++ queues/prep.queue.js
@@ -1,8 +1,7 @@
-const { Queue, QueueScheduler, QueueEvents } = require('bullmq');
+const { Queue, QueueEvents } = require('bullmq');
 const { connection } = require('./index');
 
 const prepQueue = new Queue('prep', { connection });
-new QueueScheduler('prep', { connection });
 const prepEvents = new QueueEvents('prep', { connection });
 
 module.exports = { prepQueue, prepEvents };
--- queues/train.queue.js
+++ queues/train.queue.js
@@ -1,8 +1,7 @@
-const { Queue, QueueScheduler, QueueEvents } = require('bullmq');
+const { Queue, QueueEvents } = require('bullmq');
 const { connection } = require('./index');
 
 const trainQueue = new Queue('train', { connection });
-new QueueScheduler('train', { connection });
 const trainEvents = new QueueEvents('train', { connection });
 
 module.exports = { trainQueue, trainEvents };
